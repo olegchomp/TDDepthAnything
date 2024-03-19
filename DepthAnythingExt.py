f03b7bf14fc5968be4be1289c8ae9eb50085d9f9
olegchomp <4omp@liveinternet.ru>
2024-03-19 10:59:58 +0300

    Remove dependencies

--- DepthAnythingExt.py
+++ DepthAnythingExt.py
@@ -1,11 +1,11 @@
 import os
 import cv2
 import numpy as np
-from transform import *
 import pycuda.autoinit
 import pycuda.driver as cuda
 import tensorrt as trt
 import webbrowser
+import transform
 
 class DepthAnythingExt:
 	def __init__(self, ownerComp):
@@ -17,45 +17,44 @@ class DepthAnythingExt:
 		logger = trt.Logger(trt.Logger.WARNING)
 		with open(engine, 'rb') as f, trt.Runtime(logger) as runtime:
 			engine = runtime.deserialize_cuda_engine(f.read())
-		self.context = engine.create_execution_context()
-		self.input_shape = self.context.get_tensor_shape('input')
-		self.output_shape = self.context.get_tensor_shape('output')
-		self.h_input = cuda.pagelocked_empty(trt.volume(self.input_shape), dtype=np.float32)
-		self.h_output = cuda.pagelocked_empty(trt.volume(self.output_shape), dtype=np.float32)
-		self.d_input = cuda.mem_alloc(self.h_input.nbytes)
-		self.d_output = cuda.mem_alloc(self.h_output.nbytes)
-		self.stream = cuda.Stream()
+			
+		context = engine.create_execution_context()
+		input_shape = context.get_tensor_shape('input')
+		output_shape = context.get_tensor_shape('output')
+		h_input = cuda.pagelocked_empty(trt.volume(input_shape), dtype=np.float32)
+		h_output = cuda.pagelocked_empty(trt.volume(output_shape), dtype=np.float32)
+		d_input = cuda.mem_alloc(h_input.nbytes)
+		d_output = cuda.mem_alloc(h_output.nbytes)
+		stream = cuda.Stream()
+		self.init_scripts = context, input_shape, output_shape, h_input, h_output, d_input, d_output, stream
 
 	def load_op_image(self):
 		image = op('null1').numpyArray()
 		image = cv2.cvtColor(image, cv2.COLOR_RGBA2RGB)
 		orig_shape = image.shape[:2]
-		image = transform({"image": image})["image"]  # C, H, W
+		image = transform.transform({"image": image})["image"]  # C, H, W
 		image = image[None]  # B, C, H, W
 		return image, orig_shape
 	
 	def run(self):
+		context, input_shape, output_shape, h_input, h_output, d_input, d_output, stream = self.init_scripts
 		input_image, (orig_h, orig_w) = self.load_op_image()
-	
+		
 		# Copy the input image to the pagelocked memory
-		np.copyto(self.h_input, input_image.ravel())
+		np.copyto(h_input, input_image.ravel())
 		
 		# Copy the input to the GPU, execute the inference, and copy the output back to the CPU
-		cuda.memcpy_htod_async(self.d_input, self.h_input, self.stream)
-		self.context.execute_async_v2(bindings=[int(self.d_input), int(self.d_output)], stream_handle=self.stream.handle)
-		cuda.memcpy_dtoh_async(self.h_output, self.d_output, self.stream)
-		depth = self.h_output
+		cuda.memcpy_htod_async(d_input, h_input, stream)
+		context.execute_async_v2(bindings=[int(d_input), int(d_output)], stream_handle=stream.handle)
+		cuda.memcpy_dtoh_async(h_output, d_output, stream)
+		depth = h_output
 
 		# Process the depth output
-		depth = np.reshape(depth, self.output_shape[2:])
+		depth = np.reshape(depth, output_shape[2:])
 		depth = depth.astype(np.float32) # np.uint16
 		depth = (depth - depth.min()) / (depth.max() - depth.min())
 
-		if parent().par.Output == 'grayscale':
-			image = depth[:, :, np.newaxis].astype(np.float32) # np.uint16
-		else:
-			colored_depth = cv2.applyColorMap(depth, cv2.COLORMAP_INFERNO)
-			image = cv2.cvtColor(colored_depth, cv2.COLOR_BGR2RGBA)
+		image = depth[:, :, np.newaxis].astype(np.float32) # np.uint16
 
 		return image
 		
