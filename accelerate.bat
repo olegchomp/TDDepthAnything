e5dfe397041de3b275b64a4cbf743f116023d408
olegchomp <4omp@liveinternet.ru>
2024-06-01 11:31:11 +0300

    Update accelerate.bat
    Added PyTorch version

--- accelerate.bat
+++ accelerate.bat
@@ -10,7 +10,7 @@ if not exist .venv (
     echo Installing dependencies
     pip install nvidia-cudnn-cu11==8.9.4.25 --no-cache-dir
     pip install --pre --extra-index-url https://pypi.nvidia.com tensorrt==9.0.1.post11.dev4 --no-cache-dir
-    pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+    pip install torch==2.2.2 torchvision==0.17.2 torchaudio==2.2.2 --index-url https://download.pytorch.org/whl/cu118
     echo Installing dependencies from requirements.txt...
     pip install -r requirements.txt
     pip uninstall -y nvidia-cudnn-cu11
