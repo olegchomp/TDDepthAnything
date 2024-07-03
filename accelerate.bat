4fa59e54ec1afa7a6c34cf298e9c8c7db436b8b8
olegchomp <4omp@liveinternet.ru>
2024-07-03 05:02:52 +0300

    Update accelerate.bat

--- accelerate.bat
+++ accelerate.bat
@@ -8,9 +8,6 @@ if not exist .venv (
 
     echo Activating virtual environment...
     call .venv\Scripts\activate
-    REM Replace '.venv\Scripts\activate' with path to your TouchDiffusion .venv. 
-    REM for ex. 'C:\User\TouchDiffusion\venv\Scripts\activate'
-
     echo Installing dependencies
     pip install nvidia-cudnn-cu11==8.9.4.25 --no-cache-dir
     pip install --pre --extra-index-url https://pypi.nvidia.com tensorrt==9.0.1.post11.dev4 --no-cache-dir
@@ -27,8 +24,6 @@ if not exist .venv (
 ) else (
     echo Activating virtual environment...
     call .venv\Scripts\activate.bat
-    REM Replace '.venv\Scripts\activate' with path to your TouchDiffusion .venv. 
-    REM for ex. 'C:\User\TouchDiffusion\venv\Scripts\activate'
     echo Preparing for model acceleration...
     python accelerate_model.py
 )
